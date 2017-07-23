b8590534045f5afa9d3f49264eb89406c871222c
Rob Ede <robjtede@icloud.com>
2017-07-23 19:37:36 +0100

    add redux devtools extension

--- app/scripts/index.ts
+++ app/scripts/index.ts
@@ -1,8 +1,5 @@
 import context = require('electron-contextmenu-middleware')
 import imageMenu = require('electron-image-menu')
 
-import store from '../store'
-import { Amount } from '../../lib/monzo'
-
 context.use(imageMenu)
 context.activate()
